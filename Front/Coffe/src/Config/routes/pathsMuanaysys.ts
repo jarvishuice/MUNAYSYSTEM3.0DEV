389cd624363f215e7f28cdaf88c719b6bab366d2
jarvishuice <jarvis.realg@gmal.com>
2024-04-22 13:45:23 -0400

    ajustando para los ceritifcados

--- Front/Coffe/src/Config/routes/pathsMuanaysys.ts
+++ Front/Coffe/src/Config/routes/pathsMuanaysys.ts
@@ -8,7 +8,7 @@ export class PATHMUNAYSYSY {
   // datos de conexion del API 
   private ipAPI = '191.97.17.26'; //Ip primaria 
   private ipAPIRespaldo = '190.94.248.62'; //IP de respaldo
-  private PortAPI = '8011'; //puerto de escucha del  api 
+  private PortAPI = '8443'; //puerto de escucha del  api 
 
   private CompanyName = 'nest';
 
